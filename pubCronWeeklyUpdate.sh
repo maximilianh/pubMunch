cf098b7a769327785ec19f3ba9001ac9b4dee0ed
Max <max@soe.ucsc.edu>
2017-03-05 08:21:44 -0800

    fixing uniprot update

--- pubCronWeeklyUpdate.sh
+++ pubCronWeeklyUpdate.sh
@@ -6,7 +6,7 @@ set -o errexit                        # stop on errors
 echo updating pdb
 sh /hive/data/outside/pdb/sync.sh
 echo updating uniprot
-sh /hive/data/outside/uniProtCurrent/sync.sh
+sh /hive/data/outside/uniProt/updateCurrent.sh
 echo parsing uniprot
 ~/projects/pubs/tools/pubParseDb uniprot all
 ~/projects/pubs/tools/pubParseDb uniprotTrembl all
